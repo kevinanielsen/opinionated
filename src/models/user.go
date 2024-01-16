83302ecd50d148135756e556a451d1f3d97b30d1
kevinanielsen <keveran@gmail.com>
2024-01-16 18:16:59 +0100

    chore: add votes and polls

--- src/models/user.go
+++ src/models/user.go
@@ -6,4 +6,6 @@ type User struct {
 	gorm.Model
 	HashedPassword string `json:"hashedPassword"`
 	Username       string `json:"username"`
+	Votes          []Vote `json:"votes"`
+	Polls          []Poll `json:"polls"`
 }
