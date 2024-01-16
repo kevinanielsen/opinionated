d39cfbc522debe47f149e05075fef6ec4ccd988b
kevinanielsen <keveran@gmail.com>
2024-01-16 18:16:43 +0100

    fix: change poll to pollID

--- src/models/question.go
+++ src/models/question.go
@@ -2,14 +2,16 @@ package models
 
 import "gorm.io/gorm"
 
-type Choice struct {
-	Title       string `json:"title"`
-	Description string `json:"description"`
-}
-
 type Question struct {
 	gorm.Model
 	Title   string   `json:"title"`
+	PollID  uint     `json:"pollId"`
+	Votes   []Vote   `json:"votes"`
 	Choices []Choice `json:"choices"`
-	Poll    Poll     `json:"poll"`
+}
+
+type Choice struct {
+	QuestionID  uint   `json:"questionId"`
+	Title       string `json:"title"`
+	Description string `json:"description"`
 }
