25579c65a0ff52fabc8ae212e432afe93f01997a
Kevin Nielsen <kevinanielsen@outlook.com>
2024-01-17 16:00:16 +0100

    chore(ui): remove boilerplate

--- ui/vite.config.ts
+++ ui/vite.config.ts
@@ -1,9 +1,25 @@
 import { defineConfig } from 'vite'
-import vue from '@vitejs/plugin-vue'
+import { fileURLToPath, URL } from 'node:url'
+import Vue from '@vitejs/plugin-vue'
+
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [vue()],
+  plugins: [Vue()],
+  resolve: {
+    alias: {
+      '@': fileURLToPath(new URL('./src', import.meta.url)),
+    },
+    extensions: [
+      '.js',
+      '.json',
+      '.jsx',
+      '.mjs',
+      '.ts',
+      '.tsx',
+      '.vue',
+    ],
+  },
   server: {
     proxy: {
       "/api": {target: "http://localhost:8080/", changeOrigin: true}
