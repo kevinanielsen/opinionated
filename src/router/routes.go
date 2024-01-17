9b77b35a158fbac7c087082a02a169bdaf559992
Kevin Nielsen <kevinanielsen@outlook.com>
2024-01-17 11:49:03 +0100

    feat: add ui embed

--- src/router/routes.go
+++ src/router/routes.go
@@ -3,11 +3,14 @@ package router
 import (
 	"net/http"
 
+	"github.com/kevinanielsen/opinionated/ui"
 	"github.com/labstack/echo/v4"
 )
 
 func Router() {
 	e := echo.New()
+
+	e.StaticFS("/", ui.DistDirFS)
 
 	e.GET("/api", func(c echo.Context) error {
 		return c.String(http.StatusOK, "Pong!")
